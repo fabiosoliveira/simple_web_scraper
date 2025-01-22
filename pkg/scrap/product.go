3a888e1263d0884cd08c51634ee2f9367f4d68c2
Fábio <fabio.oliveira@poscontrole.com.br>
2025-01-22 20:39:26 -0300

    ♻️ refactor(scrap): add string method to Product struct
    - add String() method to Product struct to format struct contents into a string
    - use fmt package to format string output

--- pkg/scrap/product.go
+++ pkg/scrap/product.go
@@ -1,7 +1,13 @@
 package scrap
 
+import "fmt"
+
 type Product struct {
 	Link  string
 	Name  string
 	Value float64
+}
+
+func (p Product) String() string {
+	return fmt.Sprintf("Link: %s\nName: %s\nValue: %f", p.Link, p.Name, p.Value)
 }
